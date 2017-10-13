ae32eb18030404cd9bf984910a5f812a110de6ad
Juan Henao <juanhenao21@gmail.com>
2017-10-13 17:05:15 -0500

    The first easy app

---
+++ 01_hello_world/program.py
@@ -0,0 +1,8 @@
+print('-------------------------------------------')
+print('                 Hello app')
+print('-------------------------------------------')
+print()
+
+user_text = input('What is your name? ')
+greeting = 'Nice to meet you ' + user_text
+print(greeting)
